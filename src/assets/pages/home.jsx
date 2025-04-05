968f9c12844ec07e0bdd979c9f93ce4a8a618872
olevanderheiden <olevanderheiden@hotmail.com>
2025-04-05 13:08:55 +0200

    V0.16.5

--- src/assets/pages/home.jsx
+++ src/assets/pages/home.jsx
@@ -7,7 +7,7 @@ const HomePage = () => {
   const content = (
     <div>
       <h2>Welcome to the Home Page</h2>
-      <p>This is the main content for the home page. Version 0.16.4</p>
+      <p>This is the main content for the home page. Version 0.16.5</p>
     </div>
   );
 
