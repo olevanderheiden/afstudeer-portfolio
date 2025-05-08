8fe694b510a57c0362a1badc8d06e02d4cae20d0
olevanderheiden <olevanderheiden@hotmail.com>
2025-05-08 11:01:45 +0200

    V0.60 Adding some content to the vraagstelling page and moved some styling code to the css which now allows a href elements to be standerdised through out the app.

--- src/assets/pages/chapters/chapter1.2.jsx
+++ src/assets/pages/chapters/chapter1.2.jsx
@@ -7,7 +7,17 @@ const Chapter1_2 = () => {
   const content = (
     <div>
       <h2>Probleemstelling & onderzoeksvraag</h2>
-      <p>Over Digitaal Toegankelijk</p>
+      <p>Op deze pagina vind je alles over de onderzoek vraag en deelvragen die ik heb tijdens mijn project.</p>
+      <h3>Probleemstelling</h3>
+      <p>
+        Veel bedrijven en organisaties hebben wijnig kennis of ervaring met het maken van toegankelijke websites en apps. Hier door wordt dit vaak
+        gezien als een extra kostenpost met weinig tot geen voordel, voor het bedrijf. 
+        De Europese unie heeft met de <a href="begrippen?search=EAA">EAA (European Accessibility Act)</a> een wetgeving aangenomen 
+        die bedrijven verplicht om hun websites en apps toegankelijk te maken. Deze gaat vankracht in juni 2025. Het is daarom belangrijk dat bedrijven
+        het belang en de voordelen voor alle gebruikers van toegankelijke websites en apps inzien.
+      </p>
+      <h3>Onderzoeksvraag en deel vragen</h3>
+      <p>De hoofdvraag van mijn onderzoek is: Hoe kunnen we klanten (van Digitaal Toegankelijk) laten ervaren hoe een super goed vormgegeven site/app voor audiogebruikers klinkt?</p>
     </div>
   );
 
