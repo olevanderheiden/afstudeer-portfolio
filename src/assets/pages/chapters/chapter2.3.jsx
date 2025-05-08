40c96edb9a2c58dc0897ac0f89516e896e0f0777
olevanderheiden <olevanderheiden@hotmail.com>
2025-05-08 14:28:37 +0200

    V0.62 Adding the criteria for every eind classificaite to make it easier to check if I have covered all bases when the site is complete.

--- src/assets/pages/chapters/chapter2.3.jsx
+++ src/assets/pages/chapters/chapter2.3.jsx
@@ -6,8 +6,16 @@ export const title = "Ontwikkelen";
 const Chapter2_3 = () => {
   const content = (
     <div>
-      <h2>Digitaal Toegankelijk</h2>
-      <p>Over Digitaal Toegankelijk</p>
+      <h2>Ontwikkelen</h2>
+      <ul>
+        <li>
+        Technische kennis en analyse: Je toont met je prototype, waarin de resultaten van je technische analyse gebruikt worden, aan dat deze correct en van niveau was doordat het voldoet aan de randvoorwaarden uit je conceptontwerp.
+        </li>
+        <li> 
+          Implementeren, Testen en opleveren: Je levert een iteratief ontwikkeld prototype op volgens de bedrijfsstandaarden. Je kunt de correcte werking garanderen.
+        </li>
+      </ul>
+      <p>Over het ontwikkelproces</p>
     </div>
   );
 
