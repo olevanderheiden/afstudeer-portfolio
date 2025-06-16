d24aa40957db89aa704b1d9133a4f472d4d9ef84
olevanderheiden <olevanderheiden@hotmail.com>
2025-06-16 15:36:25 +0200

    V0.103 Moving Portfolio site introductie from chapter 2.2 to chapter 0 since it has nothing todo with the actuall internship project it self.

--- src/assets/pages/chapters/chapter0.jsx
+++ src/assets/pages/chapters/chapter0.jsx
@@ -1,27 +1,47 @@
 import React from "react";
 import Layout from "../../pageElements/Layout";
 import AudioPlayer from "../../pageElements/audioPlayer";
+import ImageElement from "../../pageElements/imageElement";
 
 export const title = "Introductie";
 
 const Chapter0 = () => {
-  const sources = (
-    <>
-      <li>Source 1</li>
-      <li>Source 2</li>
-    </>
-  );
-
   const content = (
     <div>
       <h2>Hoofdstuk 0: Introducties</h2>
+      <p>
+        In deze introductie vind je een overzicht van wat je in de verschillende
+        hoofdstukken kan verwachten tevens vind je hier het ontwerp process van
+        mijn portfolio website (de site waar u momenteel bent).
+      </p>
+      <h3>Introductie stage project</h3>
       {/* Include the AudioPlayer component here */}
       <AudioPlayer fileName="test.mp3" title="Introducties (coming soon)" />
-      <p>Chapter 0 is coming here!</p>
+      <h3>Ontwerp en bouw van de portfolio website</h3>
+
+      <AudioPlayer
+        fileName="content/chapter2/2.2PortfolioSiteTourV2.mp3"
+        title="Portfolio website tour en uitleg van designkeuzes"
+      />
+      <ImageElement
+        src="content/chapter2/PortfolioSiteEersteOntwerpSchets.png"
+        title="Portfolio website ontwerpschets gemaakt April 2025"
+        alt="Dit is de ontwerpschets die ik gemaakt heb voor mijn porfolio website. Het is een hele ruwe schets zonder kleur of content op de website. Er is een menubalk bovenin met pagina's zoals: Home, Bronnen en begrippen, Aan
+        de linkerkant is een lijst van hoofdstukken te zien waarmee je naar de verschillende hoofdstukken van het portfolio kan nagiveren.
+        Het middelste gedeelte van de pagina is waar de daadwerkelijke content van de pagina te zien is. Dit kunnen afbeeldingen, teksten of audiobestanden zijn.
+        De rechterkant bevat een lijst van gebruikte bronnen bij ieder hoofdstuk. Als laatste de sectie onderaan de pagina is de footer met hierin vooral een copyright icon en referentie wie deze site heeft gemaakt. "
+      />
+      <ImageElement
+        src="content/chapter2/audOntwerpPaginaOnderzoeken.png"
+        title="Oud ontwerp voor het redesign (beluister audio opname voor meer details)"
+        alt="Een afbeelding die het oude ontwerp laat zien van de portfolio website.
+      Objecten hebben heel veel witruimte en zijn daardoor klein en haast onleesbaar
+      voor lezers!"
+      />
     </div>
   );
 
-  return <Layout content={content} sources={sources} pageTitle={title} />;
+  return <Layout content={content} />;
 };
 
 export default Chapter0;
--- src/assets/pages/chapters/chapter2.2.jsx
+++ src/assets/pages/chapters/chapter2.2.jsx
@@ -25,27 +25,7 @@ const Chapter2_2 = () => {
         Hier vind je alles over mijn ontwerpproces. Van mijn gemaakte ontwerpen
         tot het overlegproces met mijn stagebedrijf over deze ontwerpen.
       </p>
-      <h3>Ontwerpproces portfolio website</h3>
 
-      <AudioPlayer
-        fileName="content/chapter2/2.2PortfolioSiteTourV2.mp3"
-        title="Portfolio website tour en uitleg van designkeuzes"
-      />
-      <ImageElement
-        src="content/chapter2/PortfolioSiteEersteOntwerpSchets.png"
-        title="Portfolio website ontwerpschets gemaakt April 2025"
-        alt="Dit is de ontwerpschets die ik gemaakt heb voor mijn porfolio website. Het is een hele ruwe schets zonder kleur of content op de website. Er is een menubalk bovenin met pagina's zoals: Home, Bronnen en begrippen, Aan
-        de linkerkant is een lijst van hoofdstukken te zien waarmee je naar de verschillende hoofdstukken van het portfolio kan nagiveren.
-        Het middelste gedeelte van de pagina is waar de daadwerkelijke content van de pagina te zien is. Dit kunnen afbeeldingen, teksten of audiobestanden zijn.
-        De rechterkant bevat een lijst van gebruikte bronnen bij ieder hoofdstuk. Als laatste de sectie onderaan de pagina is de footer met hierin vooral een copyright icon en referentie wie deze site heeft gemaakt. "
-      />
-      <ImageElement
-        src="content/chapter2/audOntwerpPaginaOnderzoeken.png"
-        title="Oud ontwerp voor het redesign (beluister audio opname voor meer details)"
-        alt="Een afbeelding die het oude ontwerp laat zien van de portfolio website.
-      Objecten hebben heel veel witruimte en zijn daardoor klein en haast onleesbaar
-      voor lezers!"
-      />
       <h3>Ontwerpproces afstudeerproject</h3>
       <AudioPlayer
         fileName="content/chapter2/2.2uitLegOnwerpSchets1.mp3"
